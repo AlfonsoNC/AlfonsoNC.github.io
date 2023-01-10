210a1ddea01d9b675cb215bde3edef044e30e560
AlfonsoNC <93753570+AlfonsoNC@users.noreply.github.com>
2023-01-09 22:47:41 -0600

    Creando Sitio web

---
+++ paginas/js/main.js
@@ -0,0 +1,66 @@
+document.querySelector('#pdffFile').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia').setAttribute('src', pdffFileURL)
+});
+document.querySelector('#pdffFile2').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile2').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia2').setAttribute('src', pdffFileURL)
+});
+document.querySelector('#pdffFile3').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile3').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia3').setAttribute('src', pdffFileURL)
+});
+document.querySelector('#pdffFile4').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile4').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia4').setAttribute('src', pdffFileURL)
+});
+document.querySelector('#pdffFile5').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile5').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia5').setAttribute('src', pdffFileURL)
+});
+document.querySelector('#pdffFile6').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile6').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia6').setAttribute('src', pdffFileURL)
+});
+document.querySelector('#pdffFile7').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile7').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia7').setAttribute('src', pdffFileURL)
+});
+document.querySelector('#pdffFile8').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile8').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia8').setAttribute('src', pdffFileURL)
+});
+document.querySelector('#pdffFile9').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile9').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia9').setAttribute('src', pdffFileURL)
+});
+document.querySelector('#pdffFile10').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile10').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia10').setAttribute('src', pdffFileURL)
+});
+document.querySelector('#pdffFile11').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile11').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia11').setAttribute('src', pdffFileURL)
+});
+document.querySelector('#pdffFile12').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile12').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia12').setAttribute('src', pdffFileURL)
+});
+document.querySelector('#pdffFile13').addEventListener('change', ()=>{
+    let pdffFile=document.querySelector('#pdffFile13').files[0];
+    let pdffFileURL= URL.createObjectURL(pdffFile)+"#toolbar=0";
+    document.querySelector('#vistaPrevia13').setAttribute('src', pdffFileURL)
+});
+
